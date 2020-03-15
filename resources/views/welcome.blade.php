7e7236ddaa8d67c568edfd76d0dfbc0a90391f66
Paulo Rutkoski <paulo.rtkoski@gmail.com>
2020-03-15 10:22:21 -0300

    'Master'

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Font-Awesome -->
+        <link href="https://fontawesome.com/css/all.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div>Olá Mundo</div>
+    </body>
+</html>
